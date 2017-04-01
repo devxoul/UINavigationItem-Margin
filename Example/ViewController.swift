020db595cf9cbd73504bf75d21b0b4f46d5dfb41
Suyeol Jeon <devxoul@gmail.com>
2017-04-02 05:05:56 +0900

    Add example project

---
+++ Example/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Suyeol Jeon on 02/04/2017.
+//  Copyright © 2017 Suyeol Jeon. All rights reserved.
+//
+
+import UIKit
+import UINavigationItem_Margin
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.navigationItem.leftMargin = 0
+    self.navigationItem.rightMargin = 0
+  }
+
+}
+
